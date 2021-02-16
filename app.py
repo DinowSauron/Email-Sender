9a58d8869103e70e1b5b9a626515188401cb28ce
Luiz Claudio <luiz.deadzone@gmail.com>
2021-02-16 13:12:30 -0300

    Build test3

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from app import manager
+from app import app
 
 # python run.py runserver  - inicia o server
 # python run.py db init    - inicia o database
 # python run.py db migrate - faz a migração
 # python run.py db upgrade - atualiza a tabela
 if __name__ == "__main__":
-    manager.run()
+    app.run()
