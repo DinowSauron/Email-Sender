4cb0128acfc6339cf99309f5af4bb404cdca75f6
Luiz Claudio <luiz.deadzone@gmail.com>
2021-02-16 13:18:36 -0300

    Build test4

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 465
+MAIL_PORT = 4587
 MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
 MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-MAIL_USE_TLS = False
-MAIL_USE_SSL = True
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
 
 SECRET_KEY = os.environ.get("MAIL_PASSWORD")
